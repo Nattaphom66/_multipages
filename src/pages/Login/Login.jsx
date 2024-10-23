db36a9548e03e62a39e3a7e7270a8293d118fa02
Nattaphom66 <nattaphoom.mit@spumail.net>
2024-10-23 13:36:30 +0700

    UUU

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -18,7 +18,7 @@ function Login({ setToken, setRole }) {
           className="username-label bi bi-person-circle"
           htmlFor="username"
         >
-          &nbsp;Usernameaaaaa
+          &nbsp;UsernameUUUU
         </Form.Label>
         <Form.Control
           type="text"
